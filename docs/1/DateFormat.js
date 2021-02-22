b31c52742e0ab15bfc4806a1dd9a3fb388c497a7
ytyaru <ytyaru@outlook.jp>
2021-02-22 17:12:04 +0900

    time

--- docs/1/DateFormat.js
+++ docs/1/DateFormat.js
@@ -1,7 +1,7 @@
 class DateFormat {
     static #timeZone = 'Z'
     static #formatDate = '\\d{4,}[\-/]\\d{2}[\-/]\\d{2}';
-    static #formatTime = '\\d{2}:\\d{2}:\\d{2}(\.\\d+)?';
+    static #formatTime = '\\d{2}:\\d{2}(:\\d{2}(\.\\d+)?)?';
     static #formatTimeZone = '(Z|[+\-]\\d{4}|[+\-]\\d{2}:\\d{2})';
     static #formatDateTime = `${this.#formatDate}[ T]${this.#formatTime}${this.#formatTimeZone}?`;
     static #regexpDate = new RegExp(`^${this.#formatDate}$`);
@@ -13,21 +13,42 @@ class DateFormat {
              || value.match(this.#regexpTime) 
              || value.match(this.#regexpDateTime)) ? true : false;
     }
-    static convert(value) { // value:string
+    static getTimeZone(value) { return this.#timeZone; }
+    static setTimeZone(value) {
+        if (value.match(this.#formatTimeZone)) {
+            this.#timeZone = value;
+        }
+    }
+    static toType(value) { // value:string
         if (value.match(this.#formatDateTime)) {
             if (this.#hasTimeZone(value)) { return new Date(Date.parse(value)); }
             else { return new Date(Date.parse(value + this.#timeZone)); }
         }
         else if (value.match(this.#formatDate)) {
-            return new Date(Date.parse(value + '00:00:00' + this.#timeZone));
+            return new Date(Date.parse(value + 'T00:00:00' + this.#timeZone));
         }
         else if (value.match(this.#formatTime)) {
-            throw new Error(`00:00:00書式は非対応です。: ${value}`);
+//            throw new Error(`00:00:00書式は非対応です。: ${value}`);
 //            const now = new Date(); // 現在日時
             // #timeZone === 'Z' のときは UTC日付
             // #timeZone === '+0900' 等のときは Locale日付
             // それらタイムゾーンごとの日付文字列を取得する方法がわからない
 //            return new Date(Date.parse(現在日yyyy-mm-dd + value + this.#timeZone));
+            const now = new Date(); // 現在日時
+            if ('Z' === this.#timeZone) {
+                const year = `${now.getUTCFullYear()}`;
+                const month = `0${now.getUTCMonth()+1}`.slice(-2);
+                const day = `0${now.getUTCDate()}`.slice(-2);
+                console.log('UTC', `${year}-${month}-${day}T${value}${this.#timeZone}`, new Date(Date.parse(`${year}-${month}-${day}T${value}${this.#timeZone}`)));
+                return new Date(Date.parse(`${year}-${month}-${day}T${value}${this.#timeZone}`));
+                
+            } else {
+                const year = `${now.getFullYear()}`;
+                const month = `0${now.getMonth()+1}`.slice(-2);
+                const day = `0${now.getDate()}`.slice(-2);
+                console.log('Locale', `${year}-${month}-${day}T${value}${this.#timeZone}`);
+                return new Date(Date.parse(`${year}-${month}-${day}T${value}${this.#timeZone}`));
+            }
         }
         throw new Error(`非対応の書式です。: ${value}`);
     }
--- docs/1/TestDateFormat.js
+++ docs/1/TestDateFormat.js
@@ -1,17 +1,59 @@
 class TestDateFormat {
     static test() {
         this.#testIsMatch();
+        this.#testGetTimeZone();
+        this.#testToType();
     }
     static #testIsMatch() {
         console.assert(true === DateFormat.isMatch('2000-01-01'));
+        console.assert(true === DateFormat.isMatch('2000/01/01'));
+        console.assert(true === DateFormat.isMatch('2000-01-01 12:34'));
         console.assert(true === DateFormat.isMatch('2000-01-01 12:34:56'));
         console.assert(true === DateFormat.isMatch('2000-01-01 12:34:56.789'));
+        console.assert(true === DateFormat.isMatch('2000-01-01T12:34Z'));
         console.assert(true === DateFormat.isMatch('2000-01-01T12:34:56Z'));
-        console.assert(true === DateFormat.isMatch('2000-01-01T12:34:56Z'));
+        console.assert(true === DateFormat.isMatch('2000-01-01T12:34:56.789Z'));
         console.assert(true === DateFormat.isMatch('2000-01-01T12:34:56+0900'));
         console.assert(true === DateFormat.isMatch('2000-01-01T12:34:56+09:00'));
         console.assert(true === DateFormat.isMatch('2000-01-01T12:34:56-1200'));
         console.assert(true === DateFormat.isMatch('2000-01-01T12:34:56-12:00'));
+        console.assert(true === DateFormat.isMatch('2000-01-01T12:34:56.789+0900'));
+        console.assert(true === DateFormat.isMatch('2000-01-01T12:34:56.789+09:00'));
         console.assert(true === DateFormat.isMatch('12:34:56'));
+    }
+    static #testGetTimeZone() {
+        console.assert('Z' === DateFormat.getTimeZone());
+        DateFormat.setTimeZone('+0900');
+        console.assert('+0900' === DateFormat.getTimeZone());
+        DateFormat.setTimeZone('-12:00');
+        console.assert('-12:00' === DateFormat.getTimeZone());
+        DateFormat.setTimeZone('Invalid Value');
+        console.assert('-12:00' === DateFormat.getTimeZone());
+        DateFormat.setTimeZone('Z');
+        console.assert('Z' === DateFormat.getTimeZone());
+    }
+    static #testToType() {
+        console.assert(new Date('2000-01-01T00:00:00Z').getTime() === DateFormat.toType('2000-01-01').getTime());
+        console.assert(new Date('2000-01-01T00:00:00Z').getTime() === DateFormat.toType('2000-01-01 00:00').getTime());
+        console.assert(new Date('2000-01-01T00:00:00Z').getTime() === DateFormat.toType('2000-01-01 00:00:00').getTime());
+        console.assert(new Date('2000-01-01T00:00:00Z').getTime() === DateFormat.toType('2000-01-01 00:00:00.000').getTime());
+        console.assert(new Date('2000-01-01T00:00:00Z').getTime() === DateFormat.toType('2000-01-01T00:00:00Z').getTime());
+        console.assert(new Date('2000-01-01T00:00:00Z').getTime() === DateFormat.toType('2000-01-01T00:00:00.000Z').getTime());
+        const now = new Date(); // 現在日時
+        const yearUTC = `${now.getUTCFullYear()}`;
+        const monthUTC = `0${now.getUTCMonth()+1}`.slice(-2);
+        const dayUTC = `0${now.getUTCDate()}`.slice(-2);
+        const year = `${now.getFullYear()}`;
+        const month = `0${now.getMonth()+1}`.slice(-2);
+        const day = `0${now.getDate()}`.slice(-2);
+//        console.log(new Date().getTime() , DateFormat.toType('00:00').getTime());
+        console.assert(new Date(Date.parse(`${yearUTC}-${monthUTC}-${dayUTC}T00:00:00Z`)).getTime() === DateFormat.toType('00:00').getTime());
+        console.assert(new Date(Date.parse(`${yearUTC}-${monthUTC}-${dayUTC}T00:00:00Z`)).getTime() === DateFormat.toType('00:00:00').getTime());
+        console.assert(new Date(Date.parse(`${yearUTC}-${monthUTC}-${dayUTC}T00:00:00Z`)).getTime() === DateFormat.toType('00:00:00.000').getTime());
+        const tzJa = '+0900'
+        DateFormat.setTimeZone(tzJa);
+//        console.assert(new Date('2000-01-01T00:00:00+0900').getTime() === DateFormat.toType('2000-01-01').getTime());
+        console.assert(new Date(Date.parse(`${year}-${month}-${day}T00:00:00${tzJa}`)).getTime() === DateFormat.toType('00:00').getTime());
+        DateFormat.setTimeZone('Z');
     }
 }
