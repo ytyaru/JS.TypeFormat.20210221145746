03597158de0dea363adf1c2aa8c14565c399270a
ytyaru <ytyaru@outlook.jp>
2021-02-24 10:36:09 +0900

    url

---
+++ docs/4/js/typeformat/TestUrlFormat.js
@@ -0,0 +1,32 @@
+import UrlFormat from './UrlFormat.js';
+export default class TestUrlFormat {
+    static test() {
+        this.#testIsMatch();
+        this.#testToType();
+    }
+    static #testIsMatch() {
+        console.assert(true === UrlFormat.isMatch('.0'));
+        console.assert(true === UrlFormat.isMatch('0.0'));
+        console.assert(true === UrlFormat.isMatch('12.34'));
+        console.assert(true === UrlFormat.isMatch('-.5'));
+        console.assert(true === UrlFormat.isMatch('-0.5'));
+        console.assert(true === UrlFormat.isMatch('-12.34'));
+    }
+    static #assertIsMatch(value) {
+        console.assert(true === UrlFormat.isMatch(value));
+    }
+    static #assertToType(value) {
+        console.assert(new URL(value).toString() === UrlFormat.toType(value).toString());
+    }
+    static #assert(method, value) {
+        console.assert(0.0 === UrlFormat.toType('.0'));
+    }
+    static #testToType() {
+        console.assert(0.0 === UrlFormat.toType('.0'));
+        console.assert(0.0 === UrlFormat.toType('0.0'));
+        console.assert(12.34 === UrlFormat.toType('12.34'));
+        console.assert(-0.5 === UrlFormat.toType('-.5'));
+        console.assert(-0.5 === UrlFormat.toType('-0.5'));
+        console.assert(-12.34 === UrlFormat.toType('-12.34'));
+    }
+}
---
+++ docs/4/js/typeformat/UrlFormat.js
@@ -0,0 +1,11 @@
+export default class UrlFormat {
+    static #formatProtocol = '(file|http[s]?)'
+    static #regexp = new RegExp(`^${this.#formatProtocol}://$`);
+    static isMatch(value) { // value:string
+        if (value.match(this.#regexp)) { return true; }
+        return false;
+    }
+    static toType(value) { // value:string
+        return new URL(value);
+    }
+}
