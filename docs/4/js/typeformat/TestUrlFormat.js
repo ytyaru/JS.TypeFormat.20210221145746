ffd136ea8529af0b80f3a4aab0e7f7754485d602
ytyaru <ytyaru@outlook.jp>
2021-02-24 11:12:00 +0900

    TestUrlFormat

--- docs/4/js/typeformat/TestUrlFormat.js
+++ docs/4/js/typeformat/TestUrlFormat.js
@@ -1,21 +1,8 @@
 import UrlFormat from './UrlFormat.js';
 export default class TestUrlFormat {
     static test() {
-//        this.#testIsMatch();
-//        this.#testToType();
-        for (const protocol of ['http', 'https']) {
-            for (const url of [
-                `${protocol}://abc.com`,
-                `${protocol}://abc.com/`,
-                `${protocol}://abc.com/index.html`,
-                `${protocol}://abc.com/index.html#anchor`,
-                `${protocol}://abc.com/index.html?q=key%20word&key=value`
-            ]) {
-                console.log(url)
-                this.#assert(UrlFormat.isMatch, url);
-                this.#assert(UrlFormat.toType, url);
-            }
-        }
+        this.#testIsMatch();
+        this.#testToType();
     }
     static #testIsMatch() {
         console.assert(true === UrlFormat.isMatch('http://www.abc.com'));
@@ -31,44 +18,19 @@ export default class TestUrlFormat {
         console.assert(true === UrlFormat.isMatch('file:///tmp/index.html'));
     }
     static #testToType() {
-
-    }
-    static #assert(method, value) {
-        console.log(method, value, this.#getExpect(method, value), method(value))
-        console.assert(this.#getExpect(method, value) == method(value));
-    }
-    static #getExpect(method, value) {
-//        console.log(value);
-        if (method === UrlFormat.isMatch) { return true; }
-        else if (method === UrlFormat.toType) { return new URL(value).toString(); }
-    }
-    /*
-    static #assertIsMatch(value) {
-        console.assert(true === UrlFormat.isMatch(value));
-    }
-    static #assertToType(value) {
-        console.assert(new URL(value).toString() === UrlFormat.toType(value).toString());
-    }
-    static #testIsMatch() {
-        console.assert(true === UrlFormat.isMatch('.0'));
-        console.assert(true === UrlFormat.isMatch('0.0'));
-        console.assert(true === UrlFormat.isMatch('12.34'));
-        console.assert(true === UrlFormat.isMatch('-.5'));
-        console.assert(true === UrlFormat.isMatch('-0.5'));
-        console.assert(true === UrlFormat.isMatch('-12.34'));
-    }
-    static #testToType() {
         for (const protocol of ['http', 'https']) {
             for (const url of [
                 `${protocol}://abc.com`,
                 `${protocol}://abc.com/`,
                 `${protocol}://abc.com/index.html`,
                 `${protocol}://abc.com/index.html#anchor`,
-                `${protocol}://abc.com/index.html?q=key%20word&key=value`,
+                `${protocol}://abc.com/index.html?q=key%20word&key=value`
             ]) {
-            this.#assert(UrlFormat.isMatch, url);
-            this.#assert(UrlFormat.isType, url);
+                this.#assertToType(url);
+            }
         }
     }
-    */
+    static #assertToType(url) {
+        console.assert(new URL(url).toString() === UrlFormat.toType(url).toString());
+    }
 }
